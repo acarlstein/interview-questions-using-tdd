fd19e111fac8c9bfd643a60adc6ea584fa45f994
Alejandro Carlstein <acarlstein@iqn.com>
2020-02-27 18:38:54 -0700

    ts-6 -Make "amount" private-

---
+++ test/MultiCurrency/MultiCurrency.md
@@ -0,0 +1,12 @@
+# MultiCurrency
+
+* -`$5 * 2 = $10`- Done
+* -Dollar side effects?- Done
+* -equals()-
+* Make "amount" private
+* $5 + 10 CHF = $10 if rate is 2:1
+* Money rounding?
+* Make "amount" private
+* hash()
+* Equal null
+* Equal object
--- test/MultiCurrency/MultiCurrency.spec.ts
+++ test/MultiCurrency/MultiCurrency.spec.ts
@@ -6,15 +6,16 @@ describe('MultiCurrency', () => {
   it('Can we perform a multiplication?', () => {
     const five: Dollar = new Dollar(5)
     let product: Dollar = five.times(2)
-    expect(product.amount).to.be.eql(10)
+    expect(product).to.be.eql(new Dollar(10))
 
     product = five.times(3)
-    expect(product.amount).to.be.eql(15)
+    expect(product).to.be.eql(new Dollar(15))
   })
 
   it('Can we compare that are equals?', () => {
     expect(new Dollar(5).equals(new Dollar(5))).to.be.true
     expect(new Dollar(5).equals(new Dollar(6))).to.be.false
   })
+  
 
 })
